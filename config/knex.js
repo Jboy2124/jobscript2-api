1b4f2013ea8b8180905e701fdf990069247a9488
Jufel Abecia <jufelabecia@gmail.com>
2023-06-02 14:04:12 +0800

    setting env files and knex

---
+++ config/knex.js
@@ -0,0 +1,13 @@
+const knex = require('knex')({
+    client: 'mysql2',
+    connection: {
+        host: process.env.DB_HOSTNAME,
+        user: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT
+    },
+    pool: { min: 0, max: 10 }
+})
+
+module.exports = knex
